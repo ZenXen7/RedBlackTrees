8f7cd6934b380b8645b4dc2a88e699228e67cb4a
LAB14_USER ON WS25 <L14Y13W25@citulab.net>
2024-05-09 13:55:31 +0800

    new

--- redblock.h
+++ redblock.h
@@ -60,55 +60,51 @@ public:
 	
         size++;
         insertFix(curr);
+        
        	
     }
 	
 	void insertFix(node* z) {
-    while (z->parent != NULL && z->parent->isRed) {
-        if (z->parent == z->parent->parent->left) {
-            node* y = z->parent->parent->right;
-            if (y->isRed) {
-                z->parent->isRed = false;
-                y->isRed = false;
-                z->parent->parent->isRed = true;
-                z = z->parent->parent;
-            }
-            else {
-                if (z == z->parent->right) {
-                    z = z->parent;
-                    if (z->parent != NULL) {
-                        z->parent->isRed = false;
-                    }
-                    leftRotate(z);
-                }
-                z->parent->parent->isRed = true;
-                rightRotate(z->parent->parent);
-            }
-        }
-        else {
-            node* y = z->parent->parent->left;
-            if (y->isRed) {
-                z->parent->isRed = false;
-                y->isRed = false;
-                z->parent->parent->isRed = true;
-                z = z->parent->parent;
-            }
-            else {
-                if (z == z->parent->left) {
-                    z = z->parent;
-                    rightRotate(z);
-                }
-                z->parent->isRed = false;
-                z->parent->parent->isRed = true;
-                leftRotate(z->parent->parent);
-            }
-            
-            if(z == root){
-                break;
-            }
-        }
-    }
-    root->isRed = false;
+    node* u = new node;
+    while(z->parent->isRed == true){
+    	if(z->parent == z->parent->parent->right){
+    		u = z->parent->parent->left;
+    		if(u->isRed == true){
+    			u->isRed = false;
+    			z->parent->isRed = false;
+    			z->parent->parent->isRed = true;
+    			z = z->parent->parent;
+			} else {
+				if(z == z->parent->left){
+					z = z->parent;
+					rightRotate(z);
+				}
+				z->parent->isRed = false;
+				z->parent->parent->isRed = true;
+				leftRotate(z->parent->parent);
+			}
+		}else {
+			u  = z->parent->parent->right;
+			if(u->isRed == true){
+				u->isRed = false;
+				z->parent->isRed = false;
+				z->parent->parent->isRed = true;
+				z = z->parent->parent;
+			} else {
+				if(z == z->parent->right){
+					z = z->parent;
+					leftRotate(z);
+				}
+				z->parent->isRed = false;
+				z->parent->parent->isRed = true;
+				rightRotate(z->parent->parent);
+			}
+		}
+		if(z == root){
+			break;
+		}
+	}
+	root->isRed = false;
 }
 
 
